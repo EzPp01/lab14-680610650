b910d370b19dc435109d4cff49b921495a597338
Krittapat <kittapat302549@gmail.com>
2026-09-12 11:28:41 +0700

    ggg

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
